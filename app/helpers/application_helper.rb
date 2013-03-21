fd52bacc1615a397e1ecdb8608089ff0320b31ee
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-03-21 23:43:27 +0100

    Debugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Overide markdown
   def markdown string
-    string = string.gsub(/%api_hostname%/i, api_hostname)
+    string = string.gsub(/\{api_hostname\}/i, ApiBootstraper.config.api_hostname)
     super string
   end
 
